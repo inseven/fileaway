a673bd4340f66bfaf3e4269ecb0ab98c9a5283b4
Jason Morley <jason.morley@inseven.co.uk>
2018-10-11 06:37:45 -0700

    Rudimentary picker view

---
+++ Fileaway-iOS/Fileaway/PickerViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PickerViewController.swift
+//  Fileaway
+//
+//  Created by Jason Barrie Morley on 11/10/2018.
+//  Copyright © 2018 InSeven Limited. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0) {
+            return "Document"
+        } else if (section == 1) {
+            return "Kind"
+        } else if (section == 2) {
+            return "Details"
+        }
+        return nil
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 1
+        case 2:
+            return 0
+        default:
+            return 0
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+
+}
