08866646b33b7135f0229fa61119d631ad378dab
Jason Morley <jason.morley@inseven.co.uk>
2020-06-16 16:54:58 -0700

    Rename the iOS tests.

---
+++ ios/FileActionsTests/FileActionsTests.swift
@@ -0,0 +1,14 @@
+//
+//  FileActionsTests.swift
+//  FileActionsTests
+//
+//  Created by Jason Barrie Morley on 07/09/2018.
+//  Copyright © 2018 InSeven Limited. All rights reserved.
+//
+
+import XCTest
+@testable import File_Actions
+
+class FileActionsTests: XCTestCase {
+    
+}
