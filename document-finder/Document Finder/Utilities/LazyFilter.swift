155418ccc54c7745923546d1b04adbdfe3a78519
Jason Morley <jason.morley@inseven.co.uk>
2020-12-14 14:31:35 -0800

    Remove LazyFilter into Utilities

--- document-finder/Document Finder/Utilities/LazyFilter.swift
+++ document-finder/Document Finder/Utilities/LazyFilter.swift
@@ -22,14 +22,12 @@ class LazyFilter<T>: ObservableObject where T: Hashable {
     init(items: Published<[T]>.Publisher,
          test: @escaping (_ filter: String, _ item: T) -> Bool,
          initialSortDescriptor: @escaping SortDescriptor<T>) {
-        print("LazyFilter.init")
         _sortDescriptor = Published(initialValue: initialSortDescriptor)
         subscription = items
             .combineLatest($filter, $sortDescriptor)
             .debounce(for: 0.2, scheduler: DispatchQueue.main)
             .receive(on: queue)
             .map { items, filter, sortDescriptor in
-                print("regenerating list")
                 return items
                     .filter { test(filter, $0) }
                     .sorted(by: sortDescriptor)
