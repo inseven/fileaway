296962ea5566060679bad68d58d483edf24b6a0c
Jason Morley <jbmorley@mac.com>
2021-04-03 14:14:03 -0700

    Fix a build issue in the legacy FileActions workspace and enable iOS builds (#71)
    It looks like an absolute path found its way into the FileActions workspace meaning that builds wouldn't work unless the source was checked out in a very specific location. This fixes that and adds the FileActions workspace to the GitHub actions builds.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,15 +7,47 @@ set -x
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
 
-PROJECT_PATH="${ROOT_DIRECTORY}/document-finder/Fileaway.xcodeproj"
+FILEAWAY_PROJECT_PATH="${ROOT_DIRECTORY}/document-finder/Fileaway.xcodeproj"
+FILE_ACTIONS_WORKSPACE_PATH="${ROOT_DIRECTORY}/FileActions.xcworkspace"
 
 # TODO: Enable test builds if possible using a locally generated signing key.
 
-# macOS
+# Fileaway
+
+# macOS app
 xcodebuild \
-    -project "$PROJECT_PATH" \
+    -project "$FILEAWAY_PROJECT_PATH" \
     -scheme Fileaway \
     clean \
     build \
     CODE_SIGN_IDENTITY="" \
-    CODE_SIGNING_REQUIRED=NO
+    CODE_SIGNING_REQUIRED=NO | xcpretty
+
+# File Actions
+
+# iOS app
+xcodebuild \
+    -workspace "$FILE_ACTIONS_WORKSPACE_PATH" \
+    -scheme "File Actions iOS" \
+    clean \
+    build \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO | xcpretty
+
+# FileActionsCore iOS
+xcodebuild \
+    -workspace "$FILE_ACTIONS_WORKSPACE_PATH" \
+    -scheme "FileActionsCore iOS" \
+    clean \
+    build \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO | xcpretty
+
+# FileActionsCore macOS
+xcodebuild \
+    -workspace "$FILE_ACTIONS_WORKSPACE_PATH" \
+    -scheme "FileActionsCore macOS" \
+    clean \
+    build \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO | xcpretty
