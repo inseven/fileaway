e136339c5f6cef3dc83fd42320f5d8e1cebbd04a
Jason Morley <jbmorley@mac.com>
2021-04-03 17:36:23 -0700

    Add the macOS project to the workspace (#76)

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,32 +7,9 @@ set -x
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
 
-FILEAWAY_PROJECT_PATH="${ROOT_DIRECTORY}/macos/Fileaway.xcodeproj"
 FILEAWAY_WORKSPACE_PATH="${ROOT_DIRECTORY}/Fileaway.xcworkspace"
 
 # TODO: Enable test builds if possible using a locally generated signing key.
-
-# macOS Project
-
-# macOS app
-xcodebuild \
-    -project "$FILEAWAY_PROJECT_PATH" \
-    -scheme Fileaway \
-    clean \
-    build \
-    CODE_SIGN_IDENTITY="" \
-    CODE_SIGNING_REQUIRED=NO | xcpretty
-
-# Workspace
-
-# iOS app
-xcodebuild \
-    -workspace "$FILEAWAY_WORKSPACE_PATH" \
-    -scheme "File Actions iOS" \
-    clean \
-    build \
-    CODE_SIGN_IDENTITY="" \
-    CODE_SIGNING_REQUIRED=NO | xcpretty
 
 # FileActionsCore iOS
 xcodebuild \
@@ -47,6 +24,24 @@ xcodebuild \
 xcodebuild \
     -workspace "$FILEAWAY_WORKSPACE_PATH" \
     -scheme "FileActionsCore macOS" \
+    clean \
+    build \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO | xcpretty
+
+# iOS app
+xcodebuild \
+    -workspace "$FILEAWAY_WORKSPACE_PATH" \
+    -scheme "File Actions iOS" \
+    clean \
+    build \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGNING_REQUIRED=NO | xcpretty
+
+# macOS app
+xcodebuild \
+    -workspace "$FILEAWAY_WORKSPACE_PATH" \
+    -scheme "Fileaway" \
     clean \
     build \
     CODE_SIGN_IDENTITY="" \
