e41811485fb57414da296758c25088c87997d8f4
Jason Barrie Morley <jbmorley@mac.com>
2021-04-25 17:06:23 -0700

    Experimentation with a plain script.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,15 @@ set -o pipefail
 set -x
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+BUILDS_DIRECTORY="${ROOT_DIRECTORY}/builds"
+
+KEYCHAIN_PATH="${ROOT_DIRECTORY}/temporary.keychain"
+ARCHIVE_PATH="${BUILDS_DIRECTORY}/Fileaway.xcarchive"
+
+# TODO: Source the env file if it exists?
+
 
 function build_scheme {
     # Disable code signing for the build server.
@@ -28,8 +36,27 @@ function build_scheme {
 # rm -rf ~/Library/Developer/Xcode/DerivedData
 
 cd "$ROOT_DIRECTORY"
-xcodebuild -workspace Fileaway.xcworkspace -list  # List schemes
-build_scheme "FileawayCore iOS"
-build_scheme "FileawayCore macOS"
-build_scheme "Fileaway iOS"
-build_scheme "Fileaway macOS"
+
+#xcodebuild -workspace Fileaway.xcworkspace -list  # List schemes
+#build_scheme "FileawayCore iOS"
+#build_scheme "FileawayCore macOS"
+#build_scheme "Fileaway iOS"
+#build_scheme "Fileaway macOS"
+
+# Build the macOS archive.
+
+if [ -d "$BUILDS_DIRECTORY" ] ; then
+    rm -r "$BUILDS_DIRECTORY"
+fi
+mkdir -p "$BUILDS_DIRECTORY"
+
+security unlock-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+
+xcodebuild -workspace Fileaway.xcworkspace -scheme "Fileaway macOS" -config Release -archivePath "$ARCHIVE_PATH" OTHER_CODE_SIGN_FLAGS='--keychain="$KEYCHAIN_PATH"' archive
+xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILDS_DIRECTORY" -exportOptionsPlist "ExportOptions.plist"
+
+codesign -dvv "$BUILDS_DIRECTORY/Fileaway.app"
+
+pushd "$BUILDS_DIRECTORY"
+zip "Fileaway.zip" "Fileaway.app"
+popd
