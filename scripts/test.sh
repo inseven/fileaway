a5fb8abe2453425ff7a8e48723e73ce7bdbc96a9
Jason Barrie Morley <jbmorley@mac.com>
2021-04-25 20:24:56 -0700

    Adding the version number.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@
 set -e
 set -o pipefail
 set -x
+set -u
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -54,7 +55,9 @@ if [ -d "$BUILDS_DIRECTORY" ] ; then
 fi
 mkdir -p "$BUILDS_DIRECTORY"
 
-# Determine the build number.
+# Determine the version and build number.
+# TODO: Get Xcode to synthesise these itself so that builds also work there.
+VERSION_NUMBER=`cat macos/version.txt | tr -d '[:space:]'`
 GIT_COMMIT=`git rev-parse --short HEAD`
 TIMESTAMP=`date +%s`
 BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
@@ -66,16 +69,16 @@ security list-keychain -d user -s "$KEYCHAIN_PATH"
 
 # Archive and export the build.
 KEYCHAIN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\""
-xcodebuild -workspace Fileaway.xcworkspace -scheme "Fileaway macOS" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="$KEYCHAIN_FLAGS" BUILD_NUMBER='$BUILD_NUMBER' archive
+xcodebuild -workspace Fileaway.xcworkspace -scheme "Fileaway macOS" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="$KEYCHAIN_FLAGS" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive
 xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILDS_DIRECTORY" -exportOptionsPlist "ExportOptions.plist"
 
 # Show the code signing details.
 codesign -dvv "$BUILDS_DIRECTORY/Fileaway.app"
 
-# Notarize the build.
-fastlane not
+# Notarize the release build.
+fastlane notarize_release
 
 # Archive the results.
 pushd "$BUILDS_DIRECTORY"
-zip -r "Fileaway-macOS-${BUILD_NUMBER}.zip" "."
+zip -r "Fileaway-macOS-${VERSION_NUMBER}-${BUILD_NUMBER}.zip" "."
 popd
