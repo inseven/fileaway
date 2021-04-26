f35ec1df21e9683edf6d463e1c2078500165b229
Jason Morley <jbmorley@mac.com>
2021-04-26 16:28:38 -0700

    Signed and notarized automated macOS builds (#89)
    This change goes some way to automating the full release process by updating the build script to automatically sign and notarise the macOS builds. While it makes use of Fastlane's `notarize` and `match` actions, it doesn't migrate the whole build process to Fastlane (I tried that) because it just seems to add unnecessary complexity as Fastlane's trying to do too much magic under the hood and guesses wrong.
    
    Longer-term I'd like to add support for using some flavour of semantic versioning and conventional commits to auto-generate the release notes and version number.

--- README.md
+++ README.md
@@ -12,6 +12,59 @@ Document management app for iOS and macOS.
 
 Right now the macOS and iOS apps are incredibly divergent having been written with significantly different ideas of what makes for a good file management app: the iOS came first and focuses solely on the process of rule based filing, while the latest iteration of the macOS app (there have been many) also introduces a Mail-like file browser.
 
+## Development
+
+### Managing Certificates
+
+The build script (details below) uses Fastlane's match task to manage a certificates and a local keychain. Unfortunately, match seems very buggy when using Developer ID certificates, meaning it can't safely be used to fetch the certificates from Apple (it will keep creating new developer certificates until you reach your quota).
+
+Instead, you can manually import a manually created certificate and private keychain into an existing match certificate store:
+
+```bash
+fastlane match import --skip_certificate_matching true --type developer_id
+```
+
+Match will ask for the path to your certificate (`.cer`) and private key file (`.p12`).
+
+N.B. When manually importing certificates, match will not generate file names with identifiers so it's a good idea to name the certificate and private key with a matching, and obvious name.
+
+### Builds
+
+In order to make continuous integration easy the `scripts/build.sh` script builds the full project, including submitting the macOS app for notarization. In order to run this script (noting that you probably don't want to use it for regular development cycles), you'll need to configure your environment accordingly, by setting the following environment variables:
+
+- `MATCH_PASSWORD` -- the password/passphrase to secure the [match](https://docs.fastlane.tools/actions/match/) certificate store
+- `CERTIFICATE_REPOSITORY` -- the repository used for the match certificate store (must be HTTPS)
+- `CERTIFICATE_REPOSITORY_AUTHORIZATION_KEY` -- a GitHub authorization key used to access the certificate repository (see the [match authorization docs](https://docs.fastlane.tools/actions/match/#git-storage-on-github))
+- `APPLE_TEAM_ID` -- Apple Developer Team ID
+- `APPLE_DEVELOPER_ID` -- individual Apple Developer Account ID (used for notarization)
+- `FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD` -- [app-specific password](https://support.apple.com/en-us/HT204397) for the Developer Account
+
+The script (like Fastlane) will look for and source an environment file in the Fastlane directory (`Fastlane/.env`) which you can add your local details to. This file is, of course, in `.gitignore`. For example,
+
+```bash
+# Certificate store
+export MATCH_PASSWORD=
+export CERTIFICATE_REPOSITORY=
+export CERTIFICATE_REPOSITORY_AUTHORIZATION_KEY=
+
+# Developer account
+export APPLE_TEAM_ID=
+export APPLE_DEVELOPER_ID=
+export FASTLANE_APPLE_APPLICATION_SPECIFIC_PASSWORD=
+```
+
+YOu can generate your GitHub authorization key as follows:
+
+```bash
+echo -n your_github_username:your_personal_access_token | base64
+```
+
+Once you've added your environment variables to this, run the script from the root of the project directory as follows:
+
+```bash
+./scripts/build.sh
+```
+
 ## Changelog
 
 ### macOS
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 
+# Copyright (c) 2018-2021 InSeven Limited
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 set -e
 set -o pipefail
 set -x
+set -u
 
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+BUILD_DIRECTORY="${ROOT_DIRECTORY}/build"
+TEMPORARY_DIRECTORY="${ROOT_DIRECTORY}/temp"
+
+KEYCHAIN_PATH="${TEMPORARY_DIRECTORY}/temporary.keychain"
+ARCHIVE_PATH="${BUILD_DIRECTORY}/Fileaway.xcarchive"
+FASTLANE_ENV_PATH="${ROOT_DIRECTORY}/fastlane/.env"
+
+# Generate a random string to secure the local keychain.
+export TEMPORARY_KEYCHAIN_PASSWORD=`openssl rand -base64 14`
+
+# Source the Fastlane .env file if it exists to make local development easier.
+if [ -f "$FASTLANE_ENV_PATH" ] ; then
+    echo "Sourcing .env..."
+    source "$FASTLANE_ENV_PATH"
+fi
+
 
 function build_scheme {
     # Disable code signing for the build server.
-    # TODO: Enable test builds if possible using a locally generated signing key.
-    # export CODE_SIGN_IDENTITY=""
-    # export CODE_SIGNING_REQUIRED=NO
-    # export CODE_SIGNING_ALLOWED=NO
-    # export DEVELOPMENT_TEAM=""
     xcodebuild \
         -workspace Fileaway.xcworkspace \
         -scheme "$1" \
@@ -24,12 +57,64 @@ function build_scheme {
         CODE_SIGNING_ALLOWED=NO | xcpretty
 }
 
-# Clean up derived data (mostly for GitHub's benefit).
-# rm -rf ~/Library/Developer/Xcode/DerivedData
-
 cd "$ROOT_DIRECTORY"
-xcodebuild -workspace Fileaway.xcworkspace -list  # List schemes
+
+# List the available schemes
+xcodebuild -workspace Fileaway.xcworkspace -list
+
+# Smoke test builds.
+
 build_scheme "FileawayCore iOS"
 build_scheme "FileawayCore macOS"
 build_scheme "Fileaway iOS"
 build_scheme "Fileaway macOS"
+
+# Build the macOS archive.
+
+# Clean up the build directory.
+if [ -d "$BUILD_DIRECTORY" ] ; then
+    rm -r "$BUILD_DIRECTORY"
+fi
+mkdir -p "$BUILD_DIRECTORY"
+
+# Create the a new keychain.
+if [ -d "$TEMPORARY_DIRECTORY" ] ; then
+    rm -rf "$TEMPORARY_DIRECTORY"
+fi
+mkdir -p "$TEMPORARY_DIRECTORY"
+security create-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security set-keychain-settings -lut 21600 "$KEYCHAIN_PATH"
+
+# Determine the version and build number.
+# TODO: Use conventional commits and semantic versioning to auto-generate version numbers #91
+#       https://github.com/jbmorley/fileaway/issues/91
+VERSION_NUMBER=`cat macos/version.txt | tr -d '[:space:]'`
+GIT_COMMIT=`git rev-parse --short HEAD`
+TIMESTAMP=`date +%s`
+BUILD_NUMBER="${GIT_COMMIT}.${TIMESTAMP}"
+
+# Import the certificates into our dedicated keychain.
+fastlane import_certificates keychain:"$KEYCHAIN_PATH"
+security unlock-keychain -p "$TEMPORARY_KEYCHAIN_PASSWORD" "$KEYCHAIN_PATH"
+security list-keychain -d user -s "$KEYCHAIN_PATH"
+
+# Archive and export the build.
+KEYCHAIN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\""
+xcodebuild -workspace Fileaway.xcworkspace -scheme "Fileaway macOS" -config Release -archivePath "$ARCHIVE_PATH"  OTHER_CODE_SIGN_FLAGS="$KEYCHAIN_FLAGS" BUILD_NUMBER=$BUILD_NUMBER MARKETING_VERSION=$VERSION_NUMBER archive
+xcodebuild -archivePath "$ARCHIVE_PATH" -exportArchive -exportPath "$BUILD_DIRECTORY" -exportOptionsPlist "ExportOptions.plist"
+
+APP_PATH="$BUILD_DIRECTORY/Fileaway.app"
+
+# Show the code signing details.
+codesign -dvv "$APP_PATH"
+
+# Notarize the release build.
+fastlane notarize_release package:"$APP_PATH"
+
+# Archive the results.
+pushd "$BUILD_DIRECTORY"
+zip -r "Fileaway-macOS-${VERSION_NUMBER}-${BUILD_NUMBER}.zip" "."
+popd
+
+# Cleanup the temporary files and keychain.
+rm -rf "$TEMPORARY_DIRECTORY"
