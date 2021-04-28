4ccdfe4f93109f865fb07c13cc458ec27756ad70
Jason Barrie Morley <jbmorley@mac.com>
2021-04-28 02:03:42 -0700

    Better release packaging.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-changes --scope macOS release --push --command 'gh release create $CHANGES_TAG --prerelease --title "$CHANGES_TITLE" --notes "$CHANGES_NOTES"'
+changes --scope macOS release --push --command 'gh release create $CHANGES_TAG --prerelease --title "$CHANGES_TITLE" --notes "$CHANGES_NOTES"' "$@"
