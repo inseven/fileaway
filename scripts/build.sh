a93b3bd47e594edfd6df8bf4e6773eafac11d852
Jason Morley <jbmorley@mac.com>
2021-04-29 07:52:09 -0700

    ci: Introduce automated releases on main (#106)

--- scripts/build.sh
+++ scripts/build.sh
@@ -148,6 +148,6 @@ rm -rf "$TEMPORARY_DIRECTORY"
 
 # Attempt to create a version tag and publish a GitHub release.
 # This fails quietly if there's no release to be made.
-if $RELEASE ; then
+if $RELEASE || $TRY_RELEASE ; then
     "$CHANGES_SCRIPT" --scope macOS release --skip-if-empty --push --command 'gh release create $CHANGES_TAG --prerelease --title "$CHANGES_TITLE" --notes "$CHANGES_NOTES" build/Fileaway-macOS*.zip'
 fi
