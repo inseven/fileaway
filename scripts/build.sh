4f6fa39eb2da0036b575d5b4912e102bc39cda73
Jason Barrie Morley <jbmorley@mac.com>
2021-05-03 19:31:12 -0700

    Correct the variable naming.

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,9 +25,9 @@ set -o pipefail
 set -x
 set -u
 
-SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
 BUILD_DIRECTORY="${ROOT_DIRECTORY}/build"
 TEMPORARY_DIRECTORY="${ROOT_DIRECTORY}/temp"
 
