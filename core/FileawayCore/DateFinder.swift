d977752885fc9937468f2382e262a180251160f1
Jason Barrie Morley <jbmorley@mac.com>
2021-05-20 11:51:06 -0700

    Don’t remove the date cache yet

--- core/FileawayCore/DateFinder.swift
+++ core/FileawayCore/DateFinder.swift
@@ -22,6 +22,12 @@ import Foundation
 
 class DateFinder {
 
+    // TODO: Revisit the caching in DateFinder and TitleFinder #160
+    //       https://github.com/jbmorley/fileaway/issues/160
+    static var cache: NSCache<NSString, NSArray> = {
+        return NSCache()
+    }()
+
     static var dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
@@ -29,6 +35,13 @@ class DateFinder {
     }()
 
     static func dateInstances(from string: String) -> Array<DateInstance> {
+
+        // Check the cache.
+        if let dates = Self.cache.object(forKey: string as NSString) as? Array<DateInstance> {
+            return dates
+        }
+
+        // Find the dates.
         let types: NSTextCheckingResult.CheckingType = [.date]
         let detector = try! NSDataDetector(types: types.rawValue)
         let matches = detector.matches(in: string, options: [], range: NSRange(location: 0, length: string.count))
@@ -38,6 +51,10 @@ class DateFinder {
             }
             return DateInstance(date: date, range: textCheckingResult.range)
         }
+
+        // Update the cache.
+        Self.cache.setObject(dateInstances as NSArray, forKey: string as NSString)
+
         return dateInstances
     }
 
