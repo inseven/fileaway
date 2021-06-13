658b3589623409ce9ba48703c7a19484a662442c
Jason Morley <jason.morley@inseven.co.uk>
2021-06-13 15:08:15 -0700

    ci: Ensure TRY_RELEASE doesn't need to be defined for local builds (#187)
    Previously, even though it was ignored, the `TRY_RELEASE` environment variable needed to be defined as otherwise it caused an unbound variable error.

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,7 +46,7 @@ PATH=$PATH:$BUILD_TOOLS_DIRECTORY
 # Process the command line arguments.
 POSITIONAL=()
 NOTARIZE=${NOTARIZE:-false}
-RELEASE=false
+RELEASE=${TRY_RELEASE:-false}
 while [[ $# -gt 0 ]]
 do
     key="$1"
@@ -178,7 +178,7 @@ zip -r "Artifacts.zip" "."
 popd
 
 # Attempt to create a version tag and publish a GitHub release; fails quietly if there's no new release.
-if $RELEASE || $TRY_RELEASE ; then
+if $RELEASE ; then
     changes \
         --scope macOS \
         release \
