2e9ce1963e8cc50ba8e576c8af02a312ca255bbb
Jason Barrie Morley <jbmorley@mac.com>
2022-07-14 19:37:30 +0100

    Upload the correct file to TestFlight.

--- scripts/release.sh
+++ scripts/release.sh
@@ -27,15 +27,17 @@ set -x
 # This script expects the iOS IPA to be passed as the first argument, the macOS PKG as the second argument, and any
 # additional files to be attached to the GitHub release to be passed as subsequent arguments.
 
+MACOS_PACAKGE="$1"
+
 # Validate and upload the macOS build.
 xcrun altool --validate-app \
-    -f "$1" \
+    -f "$MACOS_PACKAGE" \
     --apiKey "$APPLE_API_KEY_ID" \
     --apiIssuer "$APPLE_API_KEY_ISSUER_ID" \
     --output-format json \
     --type macos
 xcrun altool --upload-app \
-    -f "$2" \
+    -f "$MACOS_PACKAGE" \
     --primary-bundle-id "uk.co.inseven.timezones" \
     --apiKey "$APPLE_API_KEY_ID" \
     --apiIssuer "$APPLE_API_KEY_ISSUER_ID" \
