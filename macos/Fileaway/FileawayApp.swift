de3bc97f0694aa600ae3ca032d51242a8f2fb2f3
Jason Barrie Morley <jbmorley@mac.com>
2022-08-23 09:46:18 +0100

    refactor: Move `VisualEffectView` into a separate file

--- macos/Fileaway/FileawayApp.swift
+++ macos/Fileaway/FileawayApp.swift
@@ -37,27 +37,6 @@ extension FocusedValues {
 
 }
 
-struct VisualEffectView: NSViewRepresentable
-{
-    let material: NSVisualEffectView.Material
-    let blendingMode: NSVisualEffectView.BlendingMode
-
-    func makeNSView(context: Context) -> NSVisualEffectView
-    {
-        let visualEffectView = NSVisualEffectView()
-        visualEffectView.material = material
-        visualEffectView.blendingMode = blendingMode
-        visualEffectView.state = NSVisualEffectView.State.active
-        return visualEffectView
-    }
-
-    func updateNSView(_ visualEffectView: NSVisualEffectView, context: Context)
-    {
-        visualEffectView.material = material
-        visualEffectView.blendingMode = blendingMode
-    }
-}
-
 @main
 struct FileawayApp: App {
 
