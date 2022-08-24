7f2d4716b9f707cc85fe60596c2810276ed4cfc8
Jason Barrie Morley <jbmorley@mac.com>
2022-08-24 09:22:39 +0100

    Convert the core library to a package

---
+++ FileawayCore/Tests/FileawayCoreTests/FileawayCoreTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FileawayCore
+
+final class FileawayCoreTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FileawayCore().text, "Hello, World!")
+    }
+}
