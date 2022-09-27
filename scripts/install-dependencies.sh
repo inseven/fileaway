33c2efff6ab7e5b69ecfaa753617a43819a54713
Jason Morley <hello@jbmorley.co.uk>
2022-09-26 21:37:57 -0700

    ci: Explicitly name the Pipenv pip sources (#288)
    This change includes a drive-by fix to use the actions runner built-in GitHub CLI instead of installing it as part of the build process.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -36,13 +36,3 @@ source "$environment_path"
 # Install the Python dependencies
 pip3 install --user pipenv
 PIPENV_PIPFILE="$changes_directory/Pipfile" pipenv install
-
-# Install the GitHub CLI
-github_cli_url="https://github.com"`curl -s -L https://github.com/cli/cli/releases/latest | grep -o -e "/.*macOS.*tar.gz"`
-if [ -d "$GITHUB_CLI_PATH" ] ; then
-    rm -r "$GITHUB_CLI_PATH"
-fi
-mkdir -p "$GITHUB_CLI_PATH"
-curl --location "$github_cli_url" --output "cli.tar.gz"
-tar --strip-components 1 -zxv -f "cli.tar.gz" -C "$GITHUB_CLI_PATH"
-unlink "cli.tar.gz"
