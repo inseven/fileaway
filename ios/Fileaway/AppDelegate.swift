906f8d44dd4520f2991578b7bed37ee764322197
Jason Morley <hello@jbmorley.co.uk>
2022-11-11 01:08:29 +0000

    refactor: Remove unused legacy iOS view controllers (#367)

--- ios/Fileaway/AppDelegate.swift
+++ ios/Fileaway/AppDelegate.swift
@@ -31,46 +31,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UIApplication.shared.delegate as! AppDelegate
     }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window?.tintColor = #colorLiteral(red: 1, green: 0.4940795302, blue: 0.4728332758, alpha: 1)
         return true
     }
 
-    func instantiateViewController(identifier: StoryboardIdentifier) -> UIViewController? {
-        guard
-            let rootViewController = window?.rootViewController,
-            let storyboard = rootViewController.storyboard else {
-                return nil
-        }
-        return storyboard.instantiateViewController(withIdentifier: identifier.rawValue)
-    }
-
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-        guard let rootViewController = window?.rootViewController else {
-            print("Unable to get the root view controller")
-            return false
-        }
-        guard let storyboard = rootViewController.storyboard else {
-            rootViewController.present("Unable to get the storyboard")
-            return false
-        }
-        guard let pickerViewController = storyboard.instantiateViewController(withIdentifier: "PickerViewController") as? PickerViewController else {
-            rootViewController.present("Unable to instantiate the picker view controller")
-            return false
-        }
-        // TODO: Double check that this is a PDF!
-        pickerViewController.manager = settings.manager!
         do {
             try url.prepareForSecureAccess()
         } catch {
-			rootViewController.present(error: error, title: "Unable to prepare for secure access")
+            window?.rootViewController?.present(error: error, title: "Unable to prepare for secure access")
             return false
         }
-        pickerViewController.documentUrl = url
-        let navigationController = UINavigationController(rootViewController: pickerViewController)
-        navigationController.modalPresentationStyle = .formSheet
-        rootViewController.present(navigationController, animated: true, completion: nil)
-        return true
+        return false
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
