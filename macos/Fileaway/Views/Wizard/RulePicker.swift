0effde9b718e64c6cf4a97ba647ab1c7cd832516
Jason Morley <hello@jbmorley.co.uk>
2022-11-22 08:45:23 +0000

    feat: Select file types on iOS (#449)
    This change also includes a drive-by refactor to use the `defaultFocus`
    modifier.

--- macos/Fileaway/Views/Wizard/RulePicker.swift
+++ macos/Fileaway/Views/Wizard/RulePicker.swift
@@ -111,11 +111,7 @@ struct RulePicker: View {
             }
         }
         .showsStackNavigationBar("Select Rule")
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                focus = .search
-            }
-        }
+        .defaultFocus($focus, .search)
         .runs(rulePickerModel)
     }
 
