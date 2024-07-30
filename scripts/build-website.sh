650fa296e8d67782fe6f6b9ded6c995bc97601a5
Jason Morley <hello@jbmorley.co.uk>
2024-07-30 07:33:35 -1000

    ci: Use GitHub Actions for Pages website deployments
    This change switches to the new GitHub Actions based Pages deployments instead of using a separate branch to deploy the website. This should keep the repository a little cleaner.

---
+++ scripts/build-website.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Copyright (c) 2024 Jason Morley
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+set -e
+set -o pipefail
+set -x
+set -u
+
+SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ROOT_DIRECTORY="${SCRIPTS_DIRECTORY}/.."
+WEBSITE_DIRECTORY="${ROOT_DIRECTORY}/docs"
+
+RELEASE_NOTES_TEMPLATE_PATH="${SCRIPTS_DIRECTORY}/release-notes.markdown"
+HISTORY_PATH="${SCRIPTS_DIRECTORY}/history.yaml"
+RELEASE_NOTES_DIRECTORY="${ROOT_DIRECTORY}/docs/release-notes"
+RELEASE_NOTES_PATH="${RELEASE_NOTES_DIRECTORY}/index.markdown"
+
+source "${SCRIPTS_DIRECTORY}/environment.sh"
+
+# Process the command line arguments.
+POSITIONAL=()
+SERVE=false
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -s|--serve)
+        SERVE=true
+        shift
+        ;;
+        *)
+        POSITIONAL+=("$1")
+        shift
+        ;;
+    esac
+done
+
+"$SCRIPTS_DIRECTORY/update-release-notes.sh"
+
+# Install the Jekyll dependencies.
+export GEM_HOME="${ROOT_DIRECTORY}/.local/ruby"
+mkdir -p "$GEM_HOME"
+export PATH="${GEM_HOME}/bin":$PATH
+gem install bundler
+cd "${WEBSITE_DIRECTORY}"
+bundle install
+
+# Build the website.
+cd "${WEBSITE_DIRECTORY}"
+if $SERVE ; then
+    bundle exec jekyll serve --watch
+else
+    bundle exec jekyll build
+fi
