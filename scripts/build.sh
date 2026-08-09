5e6ea35b510b74640aea95426dcad3239085b5a2
Jason Morley <hello@jbmorley.co.uk>
2026-08-08 19:04:50 -1000

    ci: Use mise for build-tools and changes
    This includes a drive-by change to update the Python and Ruby versions and use in-tree gems.

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,8 +46,6 @@ RELEASE_NOTES_TEMPLATE_PATH="$SCRIPTS_DIRECTORY/release-notes.html"
 IOS_XCODE_PATH=${IOS_XCODE_PATH:-/Applications/Xcode.app}
 MACOS_XCODE_PATH=${MACOS_XCODE_PATH:-/Applications/Xcode.app}
 
-source "${SCRIPTS_DIRECTORY}/environment.sh"
-
 # Check that the GitHub command is available on the path.
 which gh || (echo "GitHub cli (gh) not available on the path." && exit 1)
 
